5f0f2da2814487079ff17f4477162b3e2681cad4
AlexKashev <alexkkashev@abv.bg>
2015-06-23 18:18:06 +0300

    Update ExamBoss.cs

--- OOP-Allied-Tion-Game/RPG-Game/Characters/ExamBoss.cs
+++ OOP-Allied-Tion-Game/RPG-Game/Characters/ExamBoss.cs
@@ -1,7 +1,14 @@
-﻿namespace RPG.Characters
+namespace RPG.Characters
 {
-    public class ExamBoss : Enemy
+    public class ExamBoss : Character
     {
+        private const int BossFocus = Bug.BugFocus + Exception.ExcFocus;
+        private const int BossEnergy = 30;
+        private const char BossSym = 'B';
 
+            public ExamBoss(Position position, string name)
+                : base(position,BossSym, name, BossFocus, BossEnergy)
+            {
+            }
     }
 }
