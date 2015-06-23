c41abe0354b49e22c269a717c78506c76426ad3d
AlexKashev <alexkkashev@abv.bg>
2015-06-23 09:54:25 +0300

    Update Character.cs
    UpdateCharacter

--- OOP-Allied-Tion-Game/RPG-Game/Characters/Character.cs
+++ OOP-Allied-Tion-Game/RPG-Game/Characters/Character.cs
@@ -2,6 +2,34 @@
 {
     public abstract class Character : GameObject
     {
+        private string name;
+        private int health;
+        private int damage;
 
+        public Character(string name, int health, int damage) : base()
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Damage = damage;
+        }
+
+        public int Health { get; set; }
+        public int Damage { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw ArgumentNullException("name", "Name cannot be empty or null");
+                }
+                this.name = value;
+            }
+        }
     }
 }
