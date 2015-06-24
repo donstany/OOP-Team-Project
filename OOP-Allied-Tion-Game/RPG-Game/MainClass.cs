39db2f1251455969ea4308cee53f1b2fdb9bb643
Asen Tahchiyski <malkstor@gmail.com>
2015-06-24 10:54:30 +0300

    Class Sound added to enable playing mp3 music & sounds.

--- OOP-Allied-Tion-Game/RPG-Game/MainClass.cs
+++ OOP-Allied-Tion-Game/RPG-Game/MainClass.cs
@@ -9,9 +9,13 @@
     public class MainClass
     {
         private const string mapPath = "../../Maps/01.txt";
+        private const string musicPath = "../../Sound/Bach-Air.mp3";
 
         public static void Main()
         {
+            Sound file = new Sound(musicPath);
+            file.Play(true);
+
             MapLoader.LoadMap(mapPath);
             List<Item> allItems = MapLoader.ItemsOnMap.ToList();
             List<Character> allCharacters = MapLoader.CharactersOnMap.ToList();
---
+++ OOP-Allied-Tion-Game/RPG-Game/Sound.cs
@@ -0,0 +1,34 @@
+﻿namespace RPG
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Text;
+
+    public class Sound
+    {
+        [DllImport("winmm.dll")]
+
+        static extern int mciSendString(string mciCommand, StringBuilder buffer, int bufferSize, IntPtr callback);
+        string fileName;
+        private string Pcommand;
+
+        public void Send(string mciCommand)
+        {
+            mciSendString(mciCommand, null, 0, IntPtr.Zero);
+        }
+
+        public Sound(string fileLocation)
+        {
+            fileName = fileLocation;
+            Send("open " + fileName);
+        }
+
+        public void Play(bool loop)
+        {
+            Pcommand = "play " + fileName;
+            if (loop)
+                Pcommand += " REPEAT";
+            Send(Pcommand);
+        }
+    }
+}
